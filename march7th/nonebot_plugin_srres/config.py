f358e2421aeec251bcacefd0fdc9ef960c3be7d4
joseandluue <joseluue@outlook.com>
2024-04-24 14:08:54 +0800

    更改wiki_url

--- march7th/nonebot_plugin_srres/config.py
+++ march7th/nonebot_plugin_srres/config.py
@@ -7,7 +7,7 @@ from nonebot import get_plugin_config
 class Config(BaseModel):
     github_proxy: Optional[str] = "https://mirror.ghproxy.com"
     sr_wiki_url: Optional[str] = (
-        "https://raw.githubusercontent.com/Joseandluue/StarRailRes/master"
+        "https://github.com/Joseandluue/star-rail-atlas/blob/index"
     )
     sr_guide_url: Optional[str] = "https://raw.githubusercontent.com/Nwflower/star-rail-atlas/master"
 
